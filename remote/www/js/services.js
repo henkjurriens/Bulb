0e22e8517c29acbab3936495bb3fc11c35f898f5
hjurriens <henk.jurriens@trivento.nl>
2015-05-18 07:06:50 +0200

    sending messages

--- remote/www/js/services.js
+++ remote/www/js/services.js
@@ -1,18 +1,32 @@
 angular.module('starter.services', [])
 
 
-
 angular.module('starter.services', []).filter("hex", function() {
-  return function(number) {
+  return function(number, color) {
     if (number !== null && number !== undefined) {
       var g = parseInt(number)
       var text = g.toString(16);
+
+      if (color ===  'red') {
+        localStorage.setItem("red", text);
+      }
+
+      if (color ===  'green') {
+        localStorage.setItem("green", text);
+      }
+
+      if (color ===  'blue') {
+        localStorage.setItem("blue", text);
+      }
+
+      var rgb = localStorage.getItem("red") + localStorage.getItem("green") + localStorage.getItem("blue");
 
       var host = "ws://localhost:5000";
       var ws = new WebSocket(host);
 
       ws.onopen = function () {
-        ws.send(text + "0000");
+        console.log( "app :" + rgb);
+        ws.send(rgb);
       };
 
       var n = parseInt(number);
